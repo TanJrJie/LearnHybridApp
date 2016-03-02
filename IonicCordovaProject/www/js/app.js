085d71c6887c003ae82989a6704e4e1de1a1efeb
TanJrJie <253445915@qq.com>
2016-03-02 09:17:32 +0800

    基本理解路由，实现了登陆功能

--- IonicCordovaProject/www/js/app.js
+++ IonicCordovaProject/www/js/app.js
@@ -1,43 +1,30 @@
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
-
-.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when("", "/login");
-
-    $stateProvider
-	.state("login", {
-	    url: "/login",
-	    templateUrl: "templates/login.html"
-	})
-	.state("main", {
-	    url: "/main",
-	    templateUrl: "templates/main.html"
-	})
-    .state("nowquantity", {
-        url: "/nowquantity",
-        templateUrl: "templates/nowquantity.html"
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleLightContent();
+        }
     });
 })
 
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state("login", {url:"/", templateUrl: "templates/login.html" })
+    .state("main", { url:"main",templateUrl: "templates/Main.html" });
+
+    $urlRouterProvider.otherwise('/');
+})
+
 .controller("home", function ($scope, $state, $http) {
-    $state.go('login');
 
     $scope.loginModel = {};
 
     $scope.login = function () {
+
         $scope.loginModel = {
             userCode: $scope.loginModel.username,
             passWord: $scope.loginModel.password,
@@ -45,17 +32,15 @@ angular.module('starter', ['ionic'])
             deviceOsVersion: "5.0"
         };
 
-        $state.go('main');
-
-        //$http({
-        //    method: 'POST',
-        //    params: $scope.loginModel,
-        //    url: "http://192.168.8.173:8080/JindaERP/app/UserLogin.do"
-        //})
-        //.success(function (response, status, headers, config) {
-        //    $state.go('main');
-        //})
-        //.error(function (response, status, headers, config) {
-        //});
+        $http({
+            method: 'POST',
+            params: $scope.loginModel,
+            url: "http://192.168.8.173:8080/JindaERP/app/UserLogin.do"
+        })
+        .success(function (response, status, headers, config) {
+            $state.go('main');
+        })
+        .error(function (response, status, headers, config) {
+        });
     };
 });
