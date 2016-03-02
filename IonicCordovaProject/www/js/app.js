efb9421dc02c3330126f8c83efe1e428bd11d007
TanJrJie <253445915@qq.com>
2016-03-02 18:03:06 +0800

    完成架构

--- IonicCordovaProject/www/js/app.js
+++ IonicCordovaProject/www/js/app.js
@@ -1,5 +1,25 @@
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers','ngCordova'])
 
+//路由配置 暂时能用
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state("login", {
+        url: "/",
+        templateUrl: "templates/login.html",
+        controller: "loginCtrl"
+    })
+
+    .state("main", {
+        url: "main",
+        templateUrl: "templates/Main.html",
+        controller: "mainCtrl"
+    });
+
+    $urlRouterProvider.otherwise('/');
+})
+
+//？？
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
@@ -9,38 +29,4 @@ angular.module('starter', ['ionic'])
             StatusBar.styleLightContent();
         }
     });
-})
-
-.config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-    .state("login", {url:"/", templateUrl: "templates/login.html" })
-    .state("main", { url:"main",templateUrl: "templates/Main.html" });
-
-    $urlRouterProvider.otherwise('/');
-})
-
-.controller("home", function ($scope, $state, $http) {
-
-    $scope.loginModel = {};
-
-    $scope.login = function () {
-
-        $scope.loginModel = {
-            userCode: $scope.loginModel.username,
-            passWord: $scope.loginModel.password,
-            deviceType: "android",
-            deviceOsVersion: "5.0"
-        };
-
-        $http({
-            method: 'POST',
-            params: $scope.loginModel,
-            url: "http://192.168.8.173:8080/JindaERP/app/UserLogin.do"
-        })
-        .success(function (response, status, headers, config) {
-            $state.go('main');
-        })
-        .error(function (response, status, headers, config) {
-        });
-    };
 });
